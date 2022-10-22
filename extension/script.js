41fe9141885c94f541ba32bea9381d9a01ef01b9
Karthik Kundurthy <karthikkundurthy@gmail.com>
2022-10-22 17:44:32 -0400

    commented out stuff

--- extension/script.js
+++ extension/script.js
@@ -31,34 +31,34 @@ totalcarb = totalcarb.slice(19, totalcarb.length - 1);
 console.log(totalcarb);
 
 // Name
-food_data.name = $(".css-16ptqna")[0].innerText;
+////food_data.name = $(".css-16ptqna")[0].innerText;
 
 // Calories
-food_data.nutrition.calories = parseInt($(".css-dpfmh3-NutritionalFacts")[0].children[1].innerText.split(" ").slice(-1)[0]);
+////food_data.nutrition.calories = parseInt($(".css-dpfmh3-NutritionalFacts")[0].children[1].innerText.split(" ").slice(-1)[0]);
 
 // Other Nutrition Data
-$(".css-dpfmh3-NutritionalFacts").find(".css-2y6cy8-Category").each((idx, obj) => {
-  let nutritionText = obj.children[0].innerText;
-  let label = nutritionText.substring(0, nutritionText.lastIndexOf(" "));
-  let value = nutritionText.substring(nutritionText.lastIndexOf(" ") + 1);
-  switch(label) {
-    case "Total Fat":
-      food_data.nutrition.fat = value;
-      break;
-    case "Cholesterol":
-      food_data.nutrition.cholesterol = value;
-      break;
-    case "Sodium":
-      food_data.nutrition.sodium = value;
-      break;
-    case "Total Carbohydrate":
-      food_data.nutrition.carbs = value;
-      break;
-    case "Protein":
-      food_data.nutrition.protein = value;
-      break;
-  }
-})
+// $(".css-dpfmh3-NutritionalFacts").find(".css-2y6cy8-Category").each((idx, obj) => {
+//   let nutritionText = obj.children[0].innerText;
+//   let label = nutritionText.substring(0, nutritionText.lastIndexOf(" "));
+//   let value = nutritionText.substring(nutritionText.lastIndexOf(" ") + 1);
+//   switch(label) {
+//     case "Total Fat":
+//       food_data.nutrition.fat = value;
+//       break;
+//     case "Cholesterol":
+//       food_data.nutrition.cholesterol = value;
+//       break;
+//     case "Sodium":
+//       food_data.nutrition.sodium = value;
+//       break;
+//     case "Total Carbohydrate":
+//       food_data.nutrition.carbs = value;
+//       break;
+//     case "Protein":
+//       food_data.nutrition.protein = value;
+//       break;
+//   }
+// })
 
 console.log("FOOD DATA");
-console.log(food_data);
+// console.log(food_data);
