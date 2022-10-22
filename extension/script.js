2a6271f9ed531b856b313d30c01b3d6d26ade07e
Karthik Kundurthy <karthikkundurthy@gmail.com>
2022-10-22 17:40:24 -0400

    getting values with xpath

--- extension/script.js
+++ extension/script.js
@@ -11,6 +11,24 @@ let food_data = {
 }
 
 console.log("SCRIPT IS RUNNING");
+var protein = document.evaluate("//*[@id=\"item_details\"]/div[5]/div[2]/div/ul[2]/li[6]/span[1]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.innerText;
+protein = protein.slice(8, protein.length - 1); // should extract number
+console.log(protein);
+var cholesterol = document.evaluate("//*[@id=\"item_details\"]/div[5]/div[2]/div/ul[2]/li[3]/span[1]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.innerText;
+cholesterol = cholesterol.slice(13, cholesterol.length - 2)
+console.log(cholesterol);
+var calories = document.evaluate("//*[@id=\"item_details\"]/div[5]/div[2]/div/ul[1]/li[2]/span", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.innerText;
+calories = calories.slice(10);
+console.log(calories);
+var totalfat = document.evaluate("//*[@id=\"item_details\"]/div[5]/div[2]/div/ul[2]/li[2]/ul/li[1]/span[1]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.innerText;
+totalfat = totalfat.slice(11, totalfat.length - 1);
+console.log(totalfat);
+var sodium = document.evaluate("//*[@id=\"item_details\"]/div[5]/div[2]/div/ul[2]/li[4]/span[1]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.innerText;
+sodium = sodium.slice(8, sodium.length - 2);
+console.log(sodium);
+var totalcarb = document.evaluate("//*[@id=\"item_details\"]/div[5]/div[2]/div/ul[2]/li[5]/ul/li[1]/span[1]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.innerText;
+totalcarb = totalcarb.slice(19, totalcarb.length - 1);
+console.log(totalcarb);
 
 // Name
 food_data.name = $(".css-16ptqna")[0].innerText;
